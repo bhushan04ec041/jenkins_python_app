f6eed465bf6bd87abc6652a7ca6b1f73ac2f781c
Ubuntu <ubuntu@ip-172-31-22-62.ec2.internal>
2025-08-31 18:37:27 +0000

    year changed to 2027

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ form_html = """
         Name: <input type="text" name="name" required><br>
         <input type="submit" value="Submit">
     </form>
-    <h3>Submitted Employees for the year 2024:</h3>
+    <h3>Submitted Employees for the year 2027:</h3>
     <ul>
     {% for emp in employees %}
         <li>ID: {{ emp[0] }}, Name: {{ emp[1] }}</li>
